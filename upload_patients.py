ea8213698b712d1b6f6f28d57cefb9cd72ce9940
Raj Vansia <vansia112@gmail.com>
2020-06-14 19:46:34 -0400

    Update upload_patients.py

--- upload_patients.py
+++ upload_patients.py
@@ -2,16 +2,21 @@ import json
 import os
 import requests
 
+
+#fhir server endpoint
 URL = "http://localhost:8080/hapi-fhir-jpaserver/fhir/"
 
+#fhir server json header content
 headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
 
-bundle = "/Users/rajvansia/Documents/GitHub/synthea/output/fhir/Abdul218_Keeling57_c754e34b-b690-424d-bf48-23418380b906.json"
+#loop over all files in the output folder in order to upload each json file for each patient.
+for dirpath, dirnames, files in os.walk('/Users/rajvansia/Documents/GitHub/synthea/output/fhir'):
 
-with open(bundle, "r") as bundle_file:
-        data = bundle_file.read()
+    for file_name in files:
+        with open('/Users/rajvansia/Documents/GitHub/synthea/output/fhir/'+file_name, "r") as bundle_file:
+                data = bundle_file.read()
 
-r = requests.post(url = URL, data = data, headers = headers)
+        r = requests.post(url = URL, data = data, headers = headers)
 
-resource = r.json()
-print(json.dumps(resource, indent=2))
+        #output file name that was processed
+        print(file_name)
